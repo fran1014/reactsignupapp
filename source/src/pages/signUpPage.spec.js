852aab26d0fe1cbcc9298458ee5bbdf477e4fbf0
Franklin <71532004+fran1014@users.noreply.github.com>
2023-09-30 18:21:21 +0200

    testing first component app

--- source/src/pages/signUpPage.spec.js
+++ source/src/pages/signUpPage.spec.js
@@ -1,8 +1,12 @@
 import SignUpPage from './SignUpPage';
 import { render, screen } from '@testing-library/react';
 
-it('has header', () => {
-  render(<SignUpPage />);
-  screen.queryAllByRole('heading', { name: 'Sign Up' });
-  expect(header).toBeInTheDocument();
+describe('Sign Up Page', () => {
+  describe('Layout', () => {
+    it('has header', () => {
+      render(<SignUpPage />);
+      const header = screen.queryAllByRole('heading', { name: 'Sign Up' });
+      expect(header).toBeInTheDocument();
+    });
+  });
 });
